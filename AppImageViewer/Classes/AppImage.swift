d15d49c269db6f0b5752a13c1601da48909bc9ab
Karthik <karthik.samy@a-cti.com>
2018-01-27 20:20:31 +0530

    adds app image class

---
+++ AppImageViewer/Classes/AppImage.swift
@@ -0,0 +1,39 @@
+//
+//  AppImage.swift
+//  AppImageViewer
+//
+//  Created by Karthik on 1/27/18.
+//
+
+import UIKit
+import Foundation
+
+@objc public protocol AppImageProtocol: NSObjectProtocol {
+    
+    var index: Int { get set }
+    var underlyingImage: UIImage! { get }
+    var contentMode: UIViewContentMode { get set }
+}
+
+// MARK: - SKPhoto
+open class AppImage: NSObject, AppImageProtocol {
+    
+    open var index: Int = 0
+    open var underlyingImage: UIImage!
+    open var contentMode: UIViewContentMode = .scaleAspectFill
+    
+    // MARK: > Base Init
+    override init() {
+        super.init()
+    }
+    
+    convenience init(image: UIImage) {
+        self.init()
+        underlyingImage = image
+    }
+    
+    // MARK: - Static Function
+    public static func appImage(forImage image: UIImage) -> AppImage {
+        return AppImage(image: image)
+    }
+}
--- Example/AppImageViewer/ViewController.swift
+++ Example/AppImageViewer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AppImageViewer
 
 class ViewController: UIViewController {
 
